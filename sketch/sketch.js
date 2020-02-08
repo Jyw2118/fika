438f3f8225ff0689e370f27af17591d4b235a031
Joanne Wang <JoanneWang@dyn-160-39-131-223.dyn.columbia.edu>
2020-02-08 18:05:54 -0500

    sketch update

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,6 +1,8 @@
 function setup(){
 	createCanvas(200,200);
 	background(0)
-	let r=floor(random(5))+;
-	createIm('background/'+r+'.jpg');
+	let r =floor(random(5))+1;
+	let img = createIm('background/'+r+'.jpg');
+	img.size(windowWidth, windowHeight);
+	img.position(0,0)
 }
